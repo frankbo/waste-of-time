054e393786f151949da1bf649615d3ce7d6d922f
Frank <frank.boeddeker@gmx.net>
2018-06-13 23:17:03 +0200

    Add omdb http request

--- src/Main.elm
+++ src/Main.elm
@@ -3,18 +3,31 @@ module Main exposing (..)
 import Html exposing (Html, text, div, h1, input, ul, li)
 import Html.Attributes exposing (src, placeholder   )
 import Html.Events exposing (onInput)
+import Http
+import Json.Decode exposing (int, string, float, nullable, Decoder, list)
+import Json.Decode.Pipeline exposing (decode, required, optional, hardcoded)
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    { searchInput : String}
+    { searchInput : String,
+      searchResults : SearchResults
+    }
+
+type alias SearchResults =
+    { search: List SearchItem
+    }
+
+type alias SearchItem =
+    { title : String
+    }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( { searchInput = ""}, Cmd.none )
+    ( { searchInput = "", searchResults = (SearchResults [])}, Cmd.none )
 
 
 
@@ -23,13 +36,20 @@ init =
 
 type Msg
     = SearchInputChange String
+    | FetchSearchResult (Result Http.Error SearchResults)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
     SearchInputChange value ->
-        ({model | searchInput = value}, Cmd.none)
+        ({ model | searchInput = value }, fetchSearchResult value)
+
+    FetchSearchResult (Ok res) ->
+        ( { model | searchResults = res }, Cmd.none)
+
+    FetchSearchResult (Err _) ->
+        (model, Cmd.none)
 
 
 
@@ -39,23 +59,52 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "Waste of time"],
+        [ h1 [] [ text "Waste of time" ],
           input [placeholder "Search for your favorite Movie/Series", onInput SearchInputChange ] [],
-          (searchResults model ["hello", "world"])
+          (searchResults model)
         ]
 
 
-searchResults : Model -> List string -> Html Msg
-searchResults model results  =
-    if (String.isEmpty model.searchInput) then
+searchResults : Model -> Html Msg
+searchResults model  =
+    if ((String.isEmpty model.searchInput) && (List.isEmpty model.searchResults.search)) then
         div [] [ text "Nothing found so far. Try again" ]
     else
         div [] [
-            ul [] [
-                li [] [text "test"]
-            ]
+            ul []
+                (List.map searchItem model.searchResults.search)
+
         ]
 
+searchItem : SearchItem -> Html Msg
+searchItem item =
+    li [] [text item.title]
+
+---- HTTP ----
+
+fetchSearchResult : String -> Cmd Msg
+fetchSearchResult searchTerm =
+    let
+        apiKey = "454bff4d"
+        url =
+          "http://www.omdbapi.com/?apikey=" ++ apiKey ++ "&s=" ++ searchTerm
+
+        request =
+          Http.get url decodeSearchTerms
+    in
+        Http.send FetchSearchResult request
+
+---- HTTP DECODER ----
+
+decodeSearchTerms : Json.Decode.Decoder SearchResults
+decodeSearchTerms =
+    decode SearchResults
+        |> required "Search" (list decodeSearchTerm)
+
+decodeSearchTerm : Json.Decode.Decoder SearchItem
+decodeSearchTerm =
+    decode SearchItem
+        |> required "Title" string
 
 ---- PROGRAM ----
 
